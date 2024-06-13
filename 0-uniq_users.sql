3f99e77f88eeb40cd530e8c6cdff45c7637fea48
Prince <Prince>
2024-06-13 22:23:19 +0000

    task 0

--- 0-uniq_users.sql
+++ 0-uniq_users.sql
@@ -1,7 +1,12 @@
--- Task: creates a table called users
--- script can be executed on any database
-CREATE TABLE IF NOT EXISTS `users` (
-	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`email` VARCHAR(255) NOT NULL UNIQUE,
-	`name` VARCHAR(255)
+-- This script creates a table 'users' with the following columns:
+-- id: integer, never null, auto increment, primary key
+-- email: string (255 characters), never null, unique
+-- name: string (255 characters)
+-- If the table already exists, the script will not fail
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
